e53b660b413d526ba72fc2cafe361eea8556147e
Matteo Sandrin <matteo.sandrin@hotmail.it>
2020-10-12 12:33:37 -0400

    Add failing test

--- tests/test_lib.py
+++ tests/test_lib.py
@@ -2,3 +2,6 @@ from structoscope import echo
 
 def test_echo():
     assert echo('This is a test') == 'This is a test'
+
+def test_echo_failing():
+    assert echo('This will fail') == 'This will pass'
